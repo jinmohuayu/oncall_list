783bb09863a2b0d00a6e69c11e7021bbab67321a
jinmohuayu <yue.liu07@ele.me>
2017-09-29 11:24:58 +0800

    [misc]将项目依赖全部用govendor管理起来

---
+++ vendor/git.elenet.me/DA/oncall/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// Config 配置
+type Config struct {
+	Port  int    // 监听端口
+	Mysql string `yaml:"mysql"` // mysql 连接字符串
+}
+
+// Get 获取配置
+func (c *Config) Get() error {
+	//打开文件
+	file, err := os.OpenFile("config.yaml", os.O_RDONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//读取文件
+	buffer, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	//把yaml形式的字符串解析成struct类型
+	err = yaml.Unmarshal(buffer, c)
+	if err != nil {
+		log.Fatalf("error:%v", err)
+	}
+
+	return nil
+}
